8d41c99fecf92b31162bf7809f87c10466053e4d
Josuva .J <s9554877@gmail.com>
2025-02-05 12:41:15 +0530

    changed the prompt

--- dev.py
+++ dev.py
@@ -4,7 +4,7 @@ def set_alarm(alarm_time):
     while True:
         current_time = datetime.datetime.now().strftime("%H:%M:%S")
         if current_time == alarm_time:
-            print("Time to wake up!")
+            print("Wake up you ugly bastard")
             break
         time.sleep(1)
 alarm_time = input("Enter the alarm time in HH:MM:SS format: ")
